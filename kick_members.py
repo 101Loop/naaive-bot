8802f5e37f9f5db142196de2ceb5a78b23aea05b
Restyled.io <commits@restyled.io>
2020-10-29 08:43:44 +0000

    Restyled by autopep8

--- kick_members.py
+++ kick_members.py
@@ -59,18 +59,21 @@ def kick_member(message):
         except ApiTelegramException as err:
             # check if the message which contains `aww` sent by group owner
             if "can't remove chat owner" in err.result_json.get("description"):
-                bot.reply_to(message, "Oops, Chat Owner can use these forbidden words!")
+                bot.reply_to(
+                    message, "Oops, Chat Owner can use these forbidden words!")
             # check if the message which contains `aww` sent by group admin
             if "user is an administrator of the chat" in err.result_json.get(
                 "description"
             ):
-                bot.reply_to(message, "Chat Admins can also use these forbidden words!")
+                bot.reply_to(
+                    message, "Chat Admins can also use these forbidden words!")
             # checks if message is sent directly to bot
             elif (
                 "chat member status can't be changed in private chats"
                 in err.result_json.get("description")
             ):
-                bot.reply_to(message, "Sorry, This doesn't work in private chats!")
+                bot.reply_to(
+                    message, "Sorry, This doesn't work in private chats!")
             # log error to sentry
             else:
                 logger.error(err)
